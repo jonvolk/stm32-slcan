e3549f6b16b0624a8c5ba18bacf84c2d317dbaec
Jon <jonvolk666@gmail.com>
2021-02-20 15:56:45 -0500

    initial commit

---
+++ usart.d
@@ -0,0 +1,48 @@
+usart.o: usart.c stm32-slcan.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/cm3/nvic.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/6.3.1/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/_intsup.h /usr/include/newlib/sys/_stdint.h \
+ /usr/lib/gcc/arm-none-eabi/6.3.1/include/stdbool.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/cm3/memorymap.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/dispatch/nvic.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/f1/nvic.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/cm3/systick.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/can.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/memorymap.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/f1/memorymap.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/gpio.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/f1/gpio.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/common/gpio_common_all.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/flash.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/f1/flash.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/common/flash_common_all.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/common/flash_common_f.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/common/flash_common_f01.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/rcc.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/f1/rcc.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/usart.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/f1/usart.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/common/usart_common_f124.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/common/usart_common_all.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/iwdg.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/f1/iwdg.h \
+ /embedded/arm-cortex/src/libopencm3/include/libopencm3/stm32/common/iwdg_common_all.h \
+ /usr/include/newlib/stdio.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/sys/config.h \
+ /usr/include/newlib/machine/ieeefp.h /usr/include/newlib/sys/cdefs.h \
+ /usr/lib/gcc/arm-none-eabi/6.3.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/6.3.1/include/stdarg.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/types.h \
+ /usr/include/newlib/machine/endian.h \
+ /usr/include/newlib/machine/_endian.h /usr/include/newlib/sys/select.h \
+ /usr/include/newlib/sys/_sigset.h /usr/include/newlib/sys/_timeval.h \
+ /usr/include/newlib/sys/timespec.h /usr/include/newlib/sys/_timespec.h \
+ /usr/include/newlib/machine/types.h /usr/include/newlib/sys/stdio.h \
+ /usr/include/newlib/errno.h /usr/include/newlib/sys/errno.h
